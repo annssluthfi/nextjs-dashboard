a5831bd46ac7ec5a12a36ee0c814da6f88714363
annssluthfi <annisalthf@gmail.com>
2025-03-15 11:41:41 +0700

    initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <p>Dashboard Page</p>
+      <a href="dashboard/customers" className="text-lime-600">Go To Customer page</a>
+    </div>
+  );
+}
